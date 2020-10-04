04e4c148970a053a8fd1dca5f5e8f821a35be460
Lai Boon Hui <laiboonh@gmail.com>
2020-10-04 09:27:20 +0800

    protect topic routes

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
   alias Discuss.Repo
+  plug DiscussWeb.RequireAuthPlug when action in [:new, :create, :edit, :update, :delete]
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
